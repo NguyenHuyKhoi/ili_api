047a71a798c57ba0a656b327d6d20a0bf0909191
NguyenHuyKhoi <huykhoiqc@gmail.com>
2021-12-21 22:31:48 +0700

    setup socket

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const { createServer } = require("http")
+const { Server } = require("socket.io")
+//Config socket 
+
 var cors = require('cors')
 const authRoute = require('./src/auth/route')
 const userRoute = require('./src/user/route')
@@ -8,9 +12,22 @@ const gameRoute = require('./src/game/route')
 const collectionRoute = require('./src/collection/route')
 
 const app = express()
+const httpServer = createServer(app)
+
 dotenv.config()
 
+// Socket 
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*'
+    }
+});
 
+io.on("connection", (socket) => {
+    console.log("socket  connected :", socket.id)
+});
+
+// DB
 mongoose
     .connect(process.env.MONGO_URL, {
     })
@@ -32,6 +49,6 @@ app.use('/api/user', userRoute)
 app.use('/api/game', gameRoute)
 app.use('/api/collection', collectionRoute)
 
-app.listen( 8800, () => {
-    console.log('Backend server is running.')
+httpServer.listen( 8800, () => {
+    console.log('Backend server is running in port 8800')
 })
